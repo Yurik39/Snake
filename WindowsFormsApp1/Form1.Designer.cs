29f3b6663a78a89b26420cc386d887bd5a4f39f3
Yurik39 <yurik39@mail.ru>
2020-04-28 22:50:06 +0300

    SnakeV1.0

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameUpdate = new System.Windows.Forms.Timer(this.components);
+            this.TextTailLength = new System.Windows.Forms.Label();
+            this.TailMeter = new System.Windows.Forms.Label();
+            this.MaxTailLength = new System.Windows.Forms.Label();
+            this.TailMax = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PauseLabel = new System.Windows.Forms.Label();
+            this.LiveThree = new System.Windows.Forms.PictureBox();
+            this.LiveTwo = new System.Windows.Forms.PictureBox();
+            this.LiveOne = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.LiveThree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LiveTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LiveOne)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameUpdate
+            // 
+            this.gameUpdate.Enabled = true;
+            this.gameUpdate.Interval = 150;
+            this.gameUpdate.Tick += new System.EventHandler(this.gameUpdate_Tick);
+            // 
+            // TextTailLength
+            // 
+            resources.ApplyResources(this.TextTailLength, "TextTailLength");
+            this.TextTailLength.Name = "TextTailLength";
+            // 
+            // TailMeter
+            // 
+            resources.ApplyResources(this.TailMeter, "TailMeter");
+            this.TailMeter.Name = "TailMeter";
+            // 
+            // MaxTailLength
+            // 
+            resources.ApplyResources(this.MaxTailLength, "MaxTailLength");
+            this.MaxTailLength.Name = "MaxTailLength";
+            // 
+            // TailMax
+            // 
+            resources.ApplyResources(this.TailMax, "TailMax");
+            this.TailMax.Name = "TailMax";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // PauseLabel
+            // 
+            resources.ApplyResources(this.PauseLabel, "PauseLabel");
+            this.PauseLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PauseLabel.Name = "PauseLabel";
+            // 
+            // LiveThree
+            // 
+            this.LiveThree.Image = global::WindowsFormsApp1.Images.Live;
+            resources.ApplyResources(this.LiveThree, "LiveThree");
+            this.LiveThree.InitialImage = global::WindowsFormsApp1.Images.Live;
+            this.LiveThree.Name = "LiveThree";
+            this.LiveThree.TabStop = false;
+            // 
+            // LiveTwo
+            // 
+            this.LiveTwo.Image = global::WindowsFormsApp1.Images.Live;
+            resources.ApplyResources(this.LiveTwo, "LiveTwo");
+            this.LiveTwo.InitialImage = global::WindowsFormsApp1.Images.Live;
+            this.LiveTwo.Name = "LiveTwo";
+            this.LiveTwo.TabStop = false;
+            // 
+            // LiveOne
+            // 
+            this.LiveOne.Image = global::WindowsFormsApp1.Images.Live;
+            resources.ApplyResources(this.LiveOne, "LiveOne");
+            this.LiveOne.InitialImage = global::WindowsFormsApp1.Images.Live;
+            this.LiveOne.Name = "LiveOne";
+            this.LiveOne.TabStop = false;
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            resources.ApplyResources(this, "$this");
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PauseLabel);
+            this.Controls.Add(this.LiveThree);
+            this.Controls.Add(this.LiveTwo);
+            this.Controls.Add(this.LiveOne);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TailMax);
+            this.Controls.Add(this.MaxTailLength);
+            this.Controls.Add(this.TailMeter);
+            this.Controls.Add(this.TextTailLength);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.LiveThree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LiveTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LiveOne)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameUpdate;
+        private System.Windows.Forms.Label TextTailLength;
+        private System.Windows.Forms.Label TailMeter;
+        private System.Windows.Forms.Label MaxTailLength;
+        private System.Windows.Forms.Label TailMax;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox LiveOne;
+        private System.Windows.Forms.PictureBox LiveTwo;
+        private System.Windows.Forms.PictureBox LiveThree;
+        private System.Windows.Forms.Label PauseLabel;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        private int SnakeX, SnakeY;
+        private int SnakeDirectionX = 1, SnakeDirectionY = 1;
+        private const int CellSize = 40;
+        private Point[] Tail = new Point[3];
+        private Point[] FoodPosition = new Point[3];
+     
+        private bool isStepLocked;
+        private Bitmap SnakeHeadImage, SnakeTailImage, BackGroundImage, FoodImage, LiveXImage, LiveImage;
+        private Random random = new Random();
+        private void gameUpdate_Tick(object sender, EventArgs e)
+        {
+            PushToTail(new Point(SnakeX, SnakeY));
+
+            SnakeX += SnakeDirectionX;
+            SnakeY += SnakeDirectionY;
+            if (SnakeX > 16)
+            {
+                SnakeX = 1;
+            }
+            if (SnakeX < 1)
+            {
+                SnakeX = 16;
+            }
+
+            if (SnakeY > 16)
+            {
+                SnakeY = 1;
+            }
+            if (SnakeY < 1)
+            {
+                SnakeY = 16;
+            }
+            isStepLocked = false;
+
+
+            
+               
+            
+            
+
+            for (int j = 0; j < FoodPosition.Length; j++)
+            {
+
+                
+
+                for (int i = 0; i < Tail.Length; i++)
+                {
+                    if (SnakeX == FoodPosition[j].X && SnakeY == FoodPosition[j].Y)
+                    {
+                       
+                        FoodPosition[j].X = random.Next(1, 17);
+                        FoodPosition[j].Y = random.Next(1, 17);
+                        
+                        Array.Resize(ref Tail, Tail.Length + 1);
+
+                        TailMeter.Text = " " + Tail.Length;
+
+                    }
+
+                    if (Tail[i] == FoodPosition[j])
+                    {
+                        FoodPosition[j].X = random.Next(1, 17);
+                        FoodPosition[j].Y = random.Next(1, 17);
+                    }
+
+                    
+                }
+
+                   
+                     for (int i = 0; i < Tail.Length; i++)
+                     {
+                        if (SnakeX == Tail[i].X && SnakeY == Tail[i].Y)
+                        {
+                        
+                            Array.Resize(ref Tail, Tail.Length + i - Tail.Length);
+                            TailMeter.Text = " " + Tail.Length;
+                            
+                            
+
+                            if (LiveOne.Image == LiveXImage && LiveTwo.Image == LiveXImage)
+                            {
+                                LiveThree.Image = LiveXImage;
+                            }
+
+                            if (LiveOne.Image == LiveXImage)
+                            {
+                                LiveTwo.Image = LiveXImage;
+                            }
+
+                            if (LiveOne.Image != LiveXImage)
+                            {
+                                LiveOne.Image = LiveXImage;
+                            }
+
+                            if (LiveOne.Image == LiveXImage && LiveTwo.Image == LiveXImage && LiveThree.Image == LiveXImage)
+                            {
+                                PauseLabel.Visible = true;
+                                PauseLabel.ForeColor = Color.Red;
+                                PauseLabel.Text = "       GAME OVER!!!";
+                                gameUpdate.Interval = 100000;
+                                
+                            }
+                            
+                        }
+                        
+                     }
+                     
+
+                Refresh();
+                if (Convert.ToInt32(TailMeter.Text) >= Convert.ToInt32(TailMax.Text))
+                {
+                    TailMax.Text = " " + Tail.Length;
+                }
+            }
+                   
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (gameUpdate.Enabled == true)
+            {
+                
+
+                     switch (e.KeyCode)
+                     {
+                        case Keys.A:
+                            SetDirection(-1, 0);
+                                                   
+                            break;
+                        case Keys.D:
+                            SetDirection(1, 0);
+                        
+                            break;
+                        case Keys.W:
+                            SetDirection(0, -1);
+                        
+                            break;
+                        case Keys.S:
+                            SetDirection(0, 1);
+                        
+                            break;
+                        case Keys.Space:
+                            gameUpdate.Enabled = false;
+                            PauseLabel.Visible = true;
+                            break;
+                     
+                     }
+                
+            }
+            else
+            {
+                PauseLabel.Visible = false;
+                gameUpdate.Enabled = true;
+            }
+
+
+
+        }
+
+        private void SetDirection(int x, int y)
+        {
+            
+            if ((SnakeDirectionX * -1 == x && SnakeDirectionY * -1 == y) || isStepLocked)
+            {
+                return;
+            }
+
+
+            SnakeDirectionX = x;
+            SnakeDirectionY = y;
+
+            isStepLocked = true;
+        }
+
+        public Form1()
+        {
+            
+            CreateFood();
+            LiveXImage = WindowsFormsApp1.Images.LiveX;
+            LiveImage = WindowsFormsApp1.Images.Live;
+            BackGroundImage = WindowsFormsApp1.Images.BackGround;
+            FoodImage = WindowsFormsApp1.Images.Food;
+            SnakeHeadImage = WindowsFormsApp1.Images.SnakeHead;
+            SnakeTailImage = WindowsFormsApp1.Images.SnakeTail;
+            this.SetStyle(ControlStyles.UserPaint |
+                            ControlStyles.AllPaintingInWmPaint |
+                            ControlStyles.OptimizedDoubleBuffer, true);
+            InitializeComponent();
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+
+            graphics.DrawImage(BackGroundImage, CellSize, CellSize);
+            
+            graphics.DrawImage(SnakeHeadImage, new Rectangle(SnakeX * CellSize, SnakeY * CellSize, CellSize, CellSize));
+
+            for (int j = 0; j < FoodPosition.Length; j++)
+            {
+                
+                graphics.DrawImage(FoodImage, new Rectangle(FoodPosition[j].X * CellSize, FoodPosition[j].Y * CellSize, CellSize, CellSize)); graphics.DrawImage(FoodImage, new Rectangle(FoodPosition[j].X * CellSize, FoodPosition[j].Y * CellSize, CellSize, CellSize));
+            }
+
+            for (int i = 0; i < Tail.Length; i++)
+            {
+                
+                graphics.DrawImage(SnakeTailImage, new Rectangle(Tail[i].X * CellSize, Tail[i].Y * CellSize, CellSize, CellSize));
+                
+            }
+        }
+
+  
+       
+        private void CreateFood()
+        {
+            
+                for (int j = 0; j < FoodPosition.Length; j++)
+                {
+                
+                FoodPosition[0].X = random.Next(1, 17);
+                FoodPosition[0].Y = random.Next(1, 17);
+
+                FoodPosition[1].X = random.Next(1, 17);
+                FoodPosition[1].Y = random.Next(1, 17);
+
+                FoodPosition[2].X = random.Next(1, 17);
+                FoodPosition[2].Y = random.Next(1, 17);
+                
+                }
+            
+        }
+
+        private void PushToTail(Point point)
+        {
+
+          
+            for (int i = Tail.Length - 1; i > 0; i--)
+              {
+                  Tail[i] = Tail[i - 1];
+              }
+
+            Tail[0] = point;
+
+
+
+        }
+
+        
+           
+        
+
+    }
+}
